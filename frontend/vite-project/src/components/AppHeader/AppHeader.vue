190ff3ad9a36ce54e0553d40aa3a92ea759c2b67
Olesya Valyn <o.valyn@mkskom.ru>
2025-03-01 13:14:53 +0300

    feat: Компонент шапки

---
+++ frontend/vite-project/src/components/AppHeader/AppHeader.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="header">
+    <div @click="navigateToHome" class="header__left">
+      <AppIcon class="header__left-logo" value="logo" width="64px" height="64px" />
+      <h2 class="header__left-text">Название сервиса</h2>
+    </div>
+    <div class="header__center">
+      <h2 class="header__center-text">Название ресторана</h2>
+    </div>
+    <div class="header__right">
+      <nav class="header__right-nav">
+        <ul class="header__right-list">
+          <li>
+            <AppButton text="Залы и столы" view="outlined" />
+          </li>
+          <li>
+            <AppButton text="Сотрудники" view="outlined" />
+          </li>
+        </ul>
+        <div class="header__right-profile">
+          <img src="../../assets/admin.jpg" alt="profile" />
+        </div>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+
+import AppButton from '../AppButton/AppButton.vue';
+import AppIcon from '../AppIcon/AppIcon.vue';
+
+const router = useRouter();
+
+const navigateToHome = () => {
+  router.push({ name: 'App' });
+};
+</script>
+
+<style scoped lang="scss">
+@import './AppHeader';
+</style>
