6a4bbca8a90a13e13c62e7d98ef30e151f36feaa
Olesya Valyn <o.valyn@mkskom.ru>
2025-03-01 13:14:20 +0300

    feat: Компонент иконки

---
+++ frontend/vite-project/src/components/AppIcon/AppIcon.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="icon">
+    <svg :style="{ height, width }">
+      <use :xlink:href="`${spritePath}#${value}`" />
+    </svg>
+  </div>
+</template>
+
+<script setup>
+import spritePath from "../../assets/sprite.svg";
+defineProps({
+  value: {
+    type: String,
+    default: "",
+  },
+  width: {
+    type: String,
+    default: "24px",
+  },
+  height: {
+    type: String,
+    default: "24px",
+  },
+});
+</script>
+
+<style scoped lang="scss">
+@import "./AppIcon";
+</style>
