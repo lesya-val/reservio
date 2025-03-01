50eadf8537d7b86ecc7602232f912d64aa81caf1
Olesya Valyn <o.valyn@mkskom.ru>
2025-03-01 13:14:00 +0300

    feat: Компонент кнопки

---
+++ frontend/vite-project/src/components/AppButton/AppButton.vue
@@ -0,0 +1,42 @@
+<template>
+  <button
+    :class="['button', { disabled: disabled }, `button--${view}`]"
+    :disabled="disabled"
+    @click="$emit('click')"
+  >
+    <span v-if="$slots['icon-before']" class="icon">
+      <slot name="icon-before"></slot>
+    </span>
+
+    <span class="button-text">
+      {{ text }}
+    </span>
+
+    <span v-if="$slots['icon-after']" class="icon">
+      <slot name="icon-after"></slot>
+    </span>
+  </button>
+</template>
+
+<script setup>
+defineProps({
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+  text: {
+    type: String,
+    default: '',
+  },
+  view: {
+    type: String,
+    default: 'filled',
+  },
+});
+
+defineEmits(['click']);
+</script>
+
+<style scoped lang="scss">
+@import './AppButton';
+</style>
