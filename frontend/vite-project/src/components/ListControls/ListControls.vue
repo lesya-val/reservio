3cadffc01eadac63a559ef68410223424ddc447b
Olesya Valyn <o.valyn@mkskom.ru>
2025-03-12 20:23:20 +0300

    feat: Компонент элементов управления списком

---
+++ frontend/vite-project/src/components/ListControls/ListControls.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="list-controls">
+    <div class="list-controls__title">{{ title }}</div>
+    <Search class="list-controls__search" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import Search from "../Search/Search.vue";
+
+const props = defineProps({
+  title: {
+    type: String,
+    default: "",
+  },
+});
+
+const emit = defineEmits([]);
+</script>
+
+<style scoped lang="scss">
+@import "./ListControls";
+</style>
