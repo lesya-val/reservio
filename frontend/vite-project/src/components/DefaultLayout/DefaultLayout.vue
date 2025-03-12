b10b79d7fc8dcd2194e70fb076eb0ab2b7bd0e2e
Olesya Valyn <o.valyn@mkskom.ru>
2025-03-12 20:23:42 +0300

    feat: Компонент контейнера

---
+++ frontend/vite-project/src/components/DefaultLayout/DefaultLayout.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="layout">
+    <AppHeader />
+    <div class="layout__content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import AppHeader from "../AppHeader/AppHeader.vue";
+</script>
+
+<style scoped lang="scss">
+@import "./DefaultLayout";
+</style>
