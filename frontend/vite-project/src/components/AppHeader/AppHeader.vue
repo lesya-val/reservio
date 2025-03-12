1941925bca08dd187e4f05ec06c47af5ff54a609
Olesya Valyn <o.valyn@mkskom.ru>
2025-03-12 20:24:58 +0300

    feat: Доделать компонент хедера

--- frontend/vite-project/src/components/AppHeader/AppHeader.vue
+++ frontend/vite-project/src/components/AppHeader/AppHeader.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="header">
     <div @click="navigateToHome" class="header__left">
-      <AppIcon class="header__left-logo" value="logo" width="64px" height="64px" />
-      <h2 class="header__left-text">Название сервиса</h2>
+      <AppIcon
+        class="header__left-logo"
+        value="logo"
+        width="64px"
+        height="64px"
+      />
+      <h2 class="header__left-text">Reservio</h2>
     </div>
     <div class="header__center">
       <h2 class="header__center-text">Название ресторана</h2>
@@ -11,33 +16,36 @@
       <nav class="header__right-nav">
         <ul class="header__right-list">
           <li>
-            <AppButton text="Залы и столы" view="outlined" />
+            <AppButton view="outlined">Залы и столы</AppButton>
           </li>
           <li>
-            <AppButton text="Сотрудники" view="outlined" />
+            <AppButton view="outlined">Сотрудники</AppButton>
           </li>
         </ul>
-        <div class="header__right-profile">
-          <img src="../../assets/admin.jpg" alt="profile" />
-        </div>
+        <AppIcon
+          value="user"
+          class="header__right-profile"
+          width="32px"
+          height="32px"
+        />
       </nav>
     </div>
   </div>
 </template>
 
 <script setup>
-import { useRouter } from 'vue-router';
+import { useRouter } from "vue-router";
 
-import AppButton from '../AppButton/AppButton.vue';
-import AppIcon from '../AppIcon/AppIcon.vue';
+import AppButton from "../AppButton/AppButton.vue";
+import AppIcon from "../AppIcon/AppIcon.vue";
 
 const router = useRouter();
 
 const navigateToHome = () => {
-  router.push({ name: 'App' });
+  router.push({ name: "App" });
 };
 </script>
 
 <style scoped lang="scss">
-@import './AppHeader';
+@import "./AppHeader";
 </style>
