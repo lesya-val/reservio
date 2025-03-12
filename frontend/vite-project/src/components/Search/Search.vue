1913ccdb21bab1801054d56539fe51a39fa2babc
Olesya Valyn <o.valyn@mkskom.ru>
2025-03-12 20:22:18 +0300

    feat: Компонент поиска

---
+++ frontend/vite-project/src/components/Search/Search.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="search">
+    <AppIcon class="search__icon" value="search" width="24px" height="24px" />
+    <AppInput
+      class="search__input"
+      :value="searchValue"
+      :placeholder="'Поиск'"
+      @input="onSearchChange"
+    >
+    </AppInput>
+  </div>
+</template>
+
+<script setup>
+import AppIcon from "../AppIcon/AppIcon.vue";
+import AppInput from "../AppInput/AppInput.vue";
+</script>
+
+<style lang="scss" src="./Search.scss"></style>
