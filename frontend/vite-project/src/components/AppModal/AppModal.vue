ce66fe159b1a503203521d569eb385334c3c60e4
Olesya Valyn <o.valyn@mkskom.ru>
2025-03-12 20:24:34 +0300

    feat: Компонент модального окна

---
+++ frontend/vite-project/src/components/AppModal/AppModal.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="modal">
+    <div class="modal__box">
+      <div class="modal__title">
+        <slot name="title" />
+      </div>
+      <div class="modal__content">
+        <div class="modal__buttons">
+          <slot name="buttons" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped lang="scss">
+@import "./AppModal";
+</style>
