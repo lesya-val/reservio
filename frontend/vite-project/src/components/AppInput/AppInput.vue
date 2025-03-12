145835bc9e13804de838ccaec3d346887ea39d84
Olesya Valyn <o.valyn@mkskom.ru>
2025-03-12 20:21:43 +0300

    feat: Компонент инпута

---
+++ frontend/vite-project/src/components/AppInput/AppInput.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="input">
+    <input
+      :type="type"
+      :value="modelValue"
+      :placeholder="placeholder"
+      :disabled="disabled"
+      :class="[
+        'input__field',
+        { 'input__field--error': errorMessage },
+        { 'input__field--disabled': disabled },
+      ]"
+      @input="updateValue"
+    />
+
+    <span v-if="errorMessage" class="input__field__error">
+      {{ errorMessage }}
+    </span>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+const props = defineProps({
+  modelValue: {
+    type: [String, Number],
+    default: "",
+  },
+  placeholder: {
+    type: String,
+    default: "Введите значение",
+  },
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+  errorMessage: {
+    type: String,
+    default: "",
+  },
+});
+
+const emit = defineEmits(["update:modelValue"]);
+
+const updateValue = (event: Event) => {
+  const target = event.target as HTMLInputElement;
+  emit("update:modelValue", target.value);
+};
+</script>
+
+<style scoped lang="scss">
+@import "./AppInput";
+</style>
