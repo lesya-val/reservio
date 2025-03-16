c470f5e32d407be8702bc9503e4d23c98ba227b3
Olesya Valyn <o.valyn@mkskom.ru>
2025-03-16 17:57:36 +0300

    feat: Доб страницу авторизации

---
+++ frontend/vite-project/src/views/LoginPage/LoginPage.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="login">
+    <div @click="navigateToHome" class="login__header">
+      <AppIcon
+        class="login__header-logo"
+        value="logo"
+        width="64px"
+        height="64px"
+      />
+      <h2 class="login__header-title">Reservio</h2>
+    </div>
+    <div class="login__form-wrapper">
+      <div class="login__form">
+        <div class="login__form-title">Вход</div>
+        <div class="login__form-inputs">
+          <AppInput placeholder="Введите логин" />
+          <AppInput placeholder="Введите пароль" />
+        </div>
+        <AppButton class="login__form-button">Войти</AppButton>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import AppButton from "../../components/AppButton/AppButton.vue";
+import AppIcon from "../../components/AppIcon/AppIcon.vue";
+import AppInput from "../../components/AppInput/AppInput.vue";
+</script>
+
+<style scoped lang="scss">
+@import "./LoginPage";
+</style>
