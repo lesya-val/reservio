6fb7576f0fc4dfc3dd064b9b5c055db13616b744
Olesya Valyn <o.valyn@mkskom.ru>
2025-04-23 19:27:28 +0300

    feat: Ограничить рестораны только для админа системы

--- backend/src/restaurants/restaurants.controller.ts
+++ backend/src/restaurants/restaurants.controller.ts
@@ -6,11 +6,18 @@ import {
   Param,
   Patch,
   Delete,
+  UseGuards,
 } from "@nestjs/common";
 import { RestaurantsService } from "./restaurants.service";
 import { CreateRestaurantDto, UpdateRestaurantDto } from "./restaurants.dto";
+import { JwtAuthGuard } from "src/auth/guards/jwt-auth.guard";
+import { RolesGuard } from "src/auth/guards/roles.guard";
+import { Roles } from "src/auth/decorators/roles.decorator";
+import { Role } from "@prisma/client";
 
 @Controller("restaurants")
+@UseGuards(JwtAuthGuard, RolesGuard)
+@Roles(Role.SYSTEM_ADMIN)
 export class RestaurantsController {
   constructor(private readonly restaurantsService: RestaurantsService) {}
 
