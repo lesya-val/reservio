9d3bf8fcc1a109dd389218961c9317cc761ae288
Olesya Valyn <o.valyn@mkskom.ru>
2025-05-11 13:29:52 +0300

    feat: Настроить алиасы и пути для импорта в проекте

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,6 +13,11 @@ export default defineConfig({
       scss: {
         additionalData: `@import "src/style/colors"; @import "src/style/mixins";`,
       },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
