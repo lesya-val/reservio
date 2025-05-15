fe41236055dab8ac897c857ca03decbd031c59f1
Olesya Valyn <o.valyn@mkskom.ru>
2025-05-15 07:31:56 +0300

    feat: Реализовать API для работы со столами

---
+++ frontend/src/services/tableApi.ts
@@ -0,0 +1,68 @@
+import { Table } from "../types/index";
+import apiClient from "@/services/apiClient";
+
+const BASE_URL = "http://localhost:3000/tables";
+
+// Получить все столы
+export const getTables = async (): Promise<Table[]> => {
+  const response = await apiClient.fetchWithAuth(BASE_URL);
+  if (!response.ok) {
+    throw new Error("Не удалось загрузить столы");
+  }
+
+  return response.json();
+};
+
+// Получить стол по ID
+export const getTableById = async (id: number): Promise<Table> => {
+  const response = await apiClient.fetchWithAuth(`${BASE_URL}/${id}`);
+  if (!response.ok) {
+    throw new Error("Стол не найден");
+  }
+
+  return response.json();
+};
+
+// Добавить новый стол
+export const createTable = async (data: Partial<Table>): Promise<Table> => {
+  const response = await apiClient.fetchWithAuth(BASE_URL, {
+    method: "POST",
+    body: JSON.stringify(data),
+  });
+
+  if (!response.ok) {
+    throw new Error("Ошибка при создании стола");
+  }
+
+  return response.json();
+};
+
+// Обновить стол
+export const updateTable = async (
+  id: number,
+  data: Partial<Table>
+): Promise<Table> => {
+  const response = await apiClient.fetchWithAuth(`${BASE_URL}/${id}`, {
+    method: "PATCH",
+    body: JSON.stringify(data),
+  });
+
+  if (!response.ok) {
+    throw new Error("Ошибка при обновлении стола");
+  }
+
+  return response.json();
+};
+
+// Удалить стол
+export const deleteTable = async (id: number): Promise<void> => {
+  const response = await apiClient.fetchWithAuth(`${BASE_URL}/${id}`, {
+    method: "DELETE",
+  });
+
+  if (!response.ok) {
+    throw new Error("Ошибка при удалении стола");
+  }
+
+  return response.json();
+};
