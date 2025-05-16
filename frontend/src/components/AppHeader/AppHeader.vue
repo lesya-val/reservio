69bcf472f9542a7b24de7af3e35466339e03b1a3
Olesya Valyn <o.valyn@mkskom.ru>
2025-05-16 09:02:55 +0300

    style: Обновить стили заголовка приложения

--- frontend/src/components/AppHeader/AppHeader.vue
+++ frontend/src/components/AppHeader/AppHeader.vue
@@ -1,40 +1,44 @@
 <template>
   <header class="header">
     <div class="header__left" @click="navigateToHome">
-      <AppIcon class="header__logo" value="logo" width="64px" height="64px" />
-      <h2 class="header__title">Reservio</h2>
-    </div>
-
-    <div v-if="!isSystemAdmin" class="header__center">
-      <h2>{{ restaurantName }}</h2>
+      <div class="header__info">
+        <AppIcon class="header__logo" value="logo" width="64px" height="64px" />
+        <h2 class="header__title">Reservio</h2>
+      </div>
+      <div v-if="!isSystemAdmin">
+        <h2 class="header__restaurant">
+          <span class="header__restaurant--prefix">Ресторан</span>
+          «{{ restaurantName }}»
+        </h2>
+      </div>
     </div>
 
     <div class="header__right">
-      <nav class="header__nav">
-        <ul v-if="!isSystemAdmin" class="header__menu">
+      <nav class="header__nav nav">
+        <ul v-if="userRole !== 'EMPLOYEE'" class="nav__list">
           <li
             v-for="(item, index) in menuItems"
             :key="index"
-            :class="[
-              'nav__item',
-              { 'nav__item--active': isActive(item.route) },
-            ]"
+            class="nav__item"
+            :class="{ 'nav__item--active': isActive(item.route) }"
             @click="navigate(item.route)"
           >
-            <a>{{ item.label }}</a>
+            {{ item.label }}
           </li>
         </ul>
 
-        <div class="header__profile">
+        <div class="header__profile profile">
           <AppIcon
             value="user"
-            class="header__profile-icon"
+            class="profile__icon"
             width="32px"
             height="32px"
             @click="toggleDropdown"
           />
-          <div v-if="isDropdownOpen" class="dropdown-menu">
-            <div class="dropdown-menu__item" @click="handleLogout">Выход</div>
+
+          <div v-if="isDropdownOpen" class="profile__dropdown dropdown">
+            <!-- <div class="dropdown__item" @click="goToProfile">Профиль</div> -->
+            <div class="dropdown__item" @click="handleLogout">Выход</div>
           </div>
         </div>
       </nav>
