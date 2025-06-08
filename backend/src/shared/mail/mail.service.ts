25dbd6d484de43a8468c775483a2d2551fcfd3e7
Olesya Valyn <o.valyn@mkskom.ru>
2025-06-08 10:04:52 +0300

    feat: Добавить отправку писем с подтверждением и отказом в бронировании

--- backend/src/shared/mail/mail.service.ts
+++ backend/src/shared/mail/mail.service.ts
@@ -49,4 +49,71 @@ export class MailService {
       throw new Error("Не удалось отправить письмо");
     }
   }
+
+  async sendConfirmBookingEmail(
+    to: string,
+    bookingData: {
+      name: string;
+      restaurant: string;
+      date: string;
+      time: string;
+      guestsCount: number;
+    }
+  ): Promise<void> {
+    const mailOptions = {
+      from: this.configService.get<string>("EMAIL_USER"),
+      to,
+      subject: "Reservio",
+      html: `
+        <h2>
+          Уважаемый(-ая) ${bookingData.name}, ваша бронь в ресторан ${bookingData.restaurant} успешно подтверждена!
+        </h2>
+        <p style="font-size: 18px;"><br>Ваши данные бронирования:</p>
+        <p style="font-size: 16px;">Дата и время: <strong style="font-weight: 600;">${bookingData.date}, ${bookingData.time}</strong></p>
+        <p style="font-size: 16px;">Количество человек: <strong style="font-weight: 600;">${bookingData.guestsCount}</strong></p>
+      `,
+    };
+
+    try {
+      await this.transporter.sendMail(mailOptions);
+      console.log("Письмо успешно отправлено!");
+    } catch (error) {
+      console.error("Ошибка при отправке письма:", error);
+      throw new Error("Не удалось отправить письмо");
+    }
+  }
+
+  async sendRejectBookingEmail(
+    to: string,
+    bookingData: {
+      name: string;
+      restaurant: string;
+      date: string;
+      time: string;
+      guestsCount: number;
+    }
+  ): Promise<void> {
+    const mailOptions = {
+      from: this.configService.get<string>("EMAIL_USER"),
+      to,
+      subject: "Reservio - Отказ в бронировании",
+      html: `
+      <h2>
+        Уважаемый(-ая) ${bookingData.name}, к сожалению, ваша заявка на бронирование в ресторан ${bookingData.restaurant} отклонена.
+      </h2>
+      <p style="font-size: 18px;"><br>Детали заявки:</p>
+      <p style="font-size: 16px;">Дата и время: <strong style="font-weight: 600;">${bookingData.date}, ${bookingData.time}</strong></p>
+      <p style="font-size: 16px;">Количество человек: <strong style="font-weight: 600;">${bookingData.guestsCount}</strong></p>
+      <p style="font-size: 14px;"><br>Пожалуйста, попробуйте выбрать другое время или свяжитесь с рестораном для уточнения деталей.</p>
+    `,
+    };
+
+    try {
+      await this.transporter.sendMail(mailOptions);
+      console.log("Письмо с отказом успешно отправлено!");
+    } catch (error) {
+      console.error("Ошибка при отправке письма:", error);
+      throw new Error("Не удалось отправить письмо");
+    }
+  }
 }
